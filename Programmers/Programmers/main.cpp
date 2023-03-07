078e82ed2d77eca96eed419ce324d0b118733ef8
Korealhk <dhmklhk0190@gmail.com>
2023-03-07 11:31:16 +0900

    제목 :  소인수 분해하기  레벨 :  LV. 0
     내용 및 풀이 :
      - 소인수 분해를 위해선 가장 작은 값에서부터 나누기를 진행하도록 설계
      - 위의 내용으로 나누기 할 경우 시간복잡도는 증가함...

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -36,6 +36,45 @@ string solution(string bin1, string bin2) {
 	return answer;
 }
 
+// *** 소인수분해 ***
+//소인수분해란 어떤 수를 소수들의 곱으로 표현하는 것입니다.
+//예를 들어 12를 소인수 분해하면 2 * 2 * 3 으로 나타낼 수 있습니다. 따라서 12의 소인수는 2와 3입니다.
+//자연수 n이 매개변수로 주어질 때 n의 소인수를 오름차순으로 담은 배열을 return하도록 solution 함수를 완성해주세요.
+
+vector<int> solution(int n) {
+	vector<int> answer;
+	int num = 2;
+
+	while (true)
+	{
+		// 더이상 나눌 분해가 없을 시 반복문 stop
+		if (n == 1)
+		{
+			break;
+		}
+		// 나누기
+		else if (n%num == 0)
+		{
+			n = n / num;
+
+			auto it = find(answer.begin(), answer.end(), num);
+			if (it == answer.end())
+			{
+				// 배열에 넣을 시 값이 존재하는지 확인
+				answer.push_back(num);
+			}
+			// 나누기는 2부터
+			num = 2;
+		}
+		else
+		{	//나누기가 되지 않는 다면 나눌 값 올리기
+			num++;
+		}
+	}
+
+	return answer;
+}
+
 
 void main()
 {
