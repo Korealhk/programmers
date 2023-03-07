1f96f0ddf53366e6014e3fbb76b61aeda6bc0db4
Korealhk <dhmklhk0190@gmail.com>
2023-03-07 11:03:24 +0900

    제목 :  이진수 더하기  레벨 :  LV. 0
     내용 및 풀이 :
      - 이진수를 연산하기에 앞서 두개의 이진수의 길이를 같도록 변경
      - 자리 올림수가 발생하는지 체크하며 반복문에서 더하기를 수행
      - 반복문 종료 이후에 자리 올림이 있다면 맨 앞에 '1' 을 추가..

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -1,9 +1,45 @@
-
+﻿
 #include "main.h"
+
+// *** 이진수 더하기 ***
+//이진수를 의미하는 두 개의 문자열 bin1과 bin2가 매개변수로 주어질 때, 
+//두 이진수의 합을 return하도록 solution 함수를 완성해주세요.
+string solution(string bin1, string bin2) {
+	string answer = "";
+	int carry_bit = 0;		// 자리 올림수
+
+	// 이진 bit 길이 같도록 Set
+	if (bin1.length() < bin2.length())
+	{
+		bin1 = string(bin2.length() - bin1.length(), '0') + bin1;
+	}
+	else if (bin2.length() < bin1.length())
+	{
+		bin2 = string(bin1.length() - bin2.length(), '0') + bin2;
+	}
+
+	// 끝에서 부터 계산
+	for (int i = bin1.length()-1; i >= 0; i--)
+	{
+		int sum = stoi(to_string(bin1[i] - '0')) + stoi(to_string(bin2[i] - '0')) + carry_bit;
+		carry_bit = sum / 2;
+
+		answer = to_string(sum % 2) + answer;
+	}
+	
+	// 자리 올림수 있다면 맨앞에 1 추가
+	if (carry_bit == 1)
+	{
+		answer = '1' + answer;
+	}
+
+	return answer;
+}
+
 
 void main()
 {
-	cout << "hello World" << endl;
+	solution("100", "100");
 
 	return;
 }
