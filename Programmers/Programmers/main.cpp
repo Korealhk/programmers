fdf15e6e5a97f0e4d16ce4c7c558376556fbf107
Korealhk <dhmklhk0190@gmail.com>
2023-03-07 13:10:54 +0900

    제목 :  7의 개수  레벨 :  LV. 0
     내용 및 풀이 :
      - 배열에 들어있는값을 한자리씩 7인지 확인 후  Count 증가

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -75,10 +75,32 @@ vector<int> solution(int n) {
 	return answer;
 }
 
+//7의 개수
+//머쓱이는 행운의 숫자 7을 가장 좋아합니다.
+//정수 배열 array가 매개변수로 주어질 때, 7이 총 몇 개 있는지 return 하도록 solution 함수를 완성해보세요.
+int solution(vector<int> array) {
+	int answer = 0;
+
+	for (int n : array)
+	{
+		while (n >= 7)
+		{
+			if (n % 10 == 7)
+			{
+				answer++;
+			}
+
+			n /= 10;
+		}
+	}
+	return answer;
+}
+
 
 void main()
 {
-	solution("100", "100");
+	vector<int> array = {7, 77, 17 };
+	solution(array);
 
 	return;
 }
