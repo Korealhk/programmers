31b0edb3bfc247cc71c49c19a83b463dba584613
Korealhk <dhmklhk0190@gmail.com>
2023-03-08 16:15:20 +0900

    제목 :  문자열 계산하기  레벨 :  LV. 0
     내용 및 풀이 :
      - 문자열을 배열에 저장 후 '+', '-' 기호가 나올 시에 숫자 계산하도록 구성.

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -14,12 +14,46 @@ vector<string> solution(string my_str, int n) {
 	return answer;
 }
 
+// *** 문자열 계산하기 ***
+//my_string은 "3 + 5"처럼 문자열로 된 수식입니다.문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
+int solution(string my_string) {
+	int answer = 0;
+
+	istringstream iss(my_string);
+	vector<string> v;
+	string str;
+
+	while (iss >> str)
+	{
+		v.push_back(str);
+	}
+
+	answer = stoi(v[0]);
+
+	for (int i = 1; i < v.size();i+=2)
+	{
+		if (v[i] == "+")
+		{
+			answer = answer + stoi(v[i + 1]);
+		}
+		else if (v[i] == "-")
+		{
+			answer = answer - stoi(v[i + 1]);
+			
+		}
+
+	}
+
+	return answer;
+}
+
+
 void main()
 {
 	/*vector<int> array = { 1, 2, 3 };
 	solution(array,3);
 	*/
 
-	solution("abc1Addfggg4556b",6);
+	solution("3 + 4 - -4 + 2 + 1 + -6 + 3 - 3");
 	return;
 }
