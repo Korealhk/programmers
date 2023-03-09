65e8103646979d1a6f6577be0a2864fe899394c6
Korealhk <dhmklhk0190@gmail.com>
2023-03-09 12:44:11 +0900

    제목 :  구슬을 나누는 경우의 수  레벨 :  LV. 0
     내용 및 풀이 :
      - 서로다른 n 개 중 m개를 뽑는 경우의 수의 공식은 n!/(n-m)!*m!
      - 팩토리얼 계산의 수가 커질수 있기 때문에 범위에 맞는 변수형을 사용해야한다.

--- Programmers/Programmers/main.cpp
+++ Programmers/Programmers/main.cpp
@@ -47,6 +47,40 @@ int solution(string my_string) {
 	return answer;
 }
 
+//구슬을 나누는 경우의 수
+//머쓱이는 구슬을 친구들에게 나누어주려고 합니다.구슬은 모두 다르게 생겼습니다.
+//머쓱이가 갖고 있는 구슬의 개수 balls와 친구들에게 나누어 줄 구슬 개수 share이 매개변수로 주어질 때, 
+//balls개의 구슬 중 share개의 구슬을 고르는 가능한 모든 경우의 수를 return 하는 solution 함수를 완성해주세요.
+int solution(int balls, int share) {
+
+	if (balls > 30 || share > balls)
+	{
+		return 1;
+	}
+
+	int answer = 0;
+	long double n = 1, m = 1, s = 1;
+
+	for (int i = 1; i <= balls; i++)
+	{
+		n *= i;
+	}
+
+	for (int j = 1; j <= share; j++)
+	{
+		m *= j;
+	}
+
+	for (int k = 1; k <= balls - share; k++)
+	{
+		s *= k;
+	}
+
+	answer = n / (m*s);
+
+	return answer;
+}
+
 
 void main()
 {
@@ -54,6 +88,6 @@ void main()
 	solution(array,3);
 	*/
 
-	solution("3 + 4 - -4 + 2 + 1 + -6 + 3 - 3");
+	solution(30,1);
 	return;
 }
